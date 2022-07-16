39d5b0de96358340808b82bee794d00b836bfa7e
Rohit Bisht <rohitbst.104764@gmail.com>
2022-07-16 17:39:57 +0530

    setting and task_delete_template

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY="django-insecure-8^nwvvt6s=y%%m!zj*ln8n_va3^7x7-3_)cusv)x(vj)1b*n_o"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
